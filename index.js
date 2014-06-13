f944fa84b54f62b406c04d23d608e841f255bf75
Maciej Dudziński <elmccd@gmail.com>
2014-06-13 18:48:16 +0200

    refacor

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
  * Copyright (c) 2014 Maciej Dudziński
  * Licensed under the MIT license.
  */
+
 'use strict';
+
+
 var PLUGIN_NAME = 'gulp-uncache',
     through = require('through2'),
     gutil = require('gulp-util'),
@@ -15,7 +18,7 @@ var PLUGIN_NAME = 'gulp-uncache',
     fs = require('fs'),
     path = require('path'),
     PluginError = gutil.PluginError,
-    alreadyBeeped,
+    alreadyBeeped = false,
     changed = 0,
     skipped = 0,
     defaultConfig = {
@@ -27,8 +30,27 @@ var PLUGIN_NAME = 'gulp-uncache',
     },
     config = {};
 
-function swapValue(line, filename, append) {
+function mkdirRecursive(dir) {
+    var dirs,
+        i,
+        _path;
 
+    if (fs.existsSync(path.normalize(dir))) {
+        return false;
+    }
+
+    dirs = path.normalize(dir).split(path.sep);
+
+    for (i = 0; i < dirs.length; i += 1) {
+        _path = path.normalize(dirs.slice(0, i + 1).join('/'));
+        if (!fs.existsSync(_path)) {
+            fs.mkdirSync(_path);
+        }
+    }
+}
+
+
+function swapValue(line, filename, append) {
     if (config.rename) {
         var opt = {};
         opt.name = filename.split('/').reverse()[0].split('.')[0];
@@ -106,8 +128,9 @@ function replaceFileName(line, append) {
 }
 
 function proceed(content, params) {
-
     var parts = content.split(/<!--\s*uncache\s*(\(.*\))?\s*-->/),
+        parts2,
+        lines,
         output = [],
         newLine,
         element,
@@ -122,10 +145,7 @@ function proceed(content, params) {
     //foreach block
     for(var i = 0; i < parts.length; i++){
         element = parts[i];
-
-
         if (i % 2 === 0) {
-
             //extend config
             config = util._extend({}, defaultConfig);
 
@@ -135,29 +155,27 @@ function proceed(content, params) {
                     config[param] = params[param];
                 }
             }
-
             //extend config from processing file
-            if(!element) {
-                continue;
-            }
-            configParts = element.substr(1, element.length - 2).split(',');
+            if(element) {
+                configParts = element.substr(1, element.length - 2).split(',');
 
-            configParts.forEach(function (el) {
-                configPart = el.split(':');
-                newValue = configPart[1].trim();
-                if (newValue === 'true') {
-                    newValue = true;
-                }
-                if (newValue === 'false') {
-                    newValue = false;
-                }
-                config[configPart[0].trim()] = newValue;
-            });
+                configParts.forEach(function (el) {
+                    configPart = el.split(':');
+                    newValue = configPart[1].trim();
+                    if (newValue === 'true') {
+                        newValue = true;
+                    }
+                    if (newValue === 'false') {
+                        newValue = false;
+                    }
+                    config[configPart[0].trim()] = newValue;
+                });
+            }
             continue;
         }
 
-        var parts2 = element.split(/<!--\s*enduncache\s*-->/);
-        var lines = parts2[0].split('\n');
+        parts2 = element.split(/<!--\s*enduncache\s*-->/);
+        lines = parts2[0].split('\n');
 
         lines.forEach(function (element) {
             newLine = replaceFileName(element, config.append);
@@ -172,35 +190,13 @@ function proceed(content, params) {
     return output.join('');
 }
 
-function mkdirRecursive(dir) {
-    var dirs,
-        i,
-        _path;
-
-    if (fs.existsSync(path.normalize(dir))) {
-        return false;
-    }
-
-    dirs = path.normalize(dir).split(path.sep);
-
-    for (i = 0; i < dirs.length; i += 1) {
-        _path = path.normalize(dirs.slice(0, i + 1).join('/'));
-        if (!fs.existsSync(_path)) {
-            fs.mkdirSync(_path);
-        }
-    }
-}
-
 // Plugin level function(dealing with files)
 function unCache(params) {
 
-    alreadyBeeped = false;
-    changed = 0;
-
-
     // Creating a stream through which each file will pass
-    var stream = through.obj(function (file, enc, callback) {
+    return through.obj(function (file, enc, callback) {
         var fileName = file.path;
+
         if (file.isNull()) {
             // Do nothing if no contents
         }
@@ -215,16 +211,14 @@ function unCache(params) {
         this.push(file);
 
         gutil.log(gutil.colors.green(PLUGIN_NAME), fileName + ", Changed lines: ", gutil.colors.cyan(changed));
+
         if(skipped) {
             gutil.log(gutil.colors.green(PLUGIN_NAME),  fileName + ", Skipped lines: ", gutil.colors.red(skipped));
             gutil.beep();
         }
 
-
         return callback();
     });
-    // returning the file stream
-    return stream;
 }
 
 // Exporting the plugin main function
